1edb55d9f338013a01cbef3aa130542407f98f6a
Dietrich <ldietrichl@ya.ru>
2024-05-01 20:07:26 +0300

    Ex4: GET-запрос

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,14 +4,15 @@ import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
+
     @Test
     public void testHelloWorld(){
-            System.out.println("Hello from Dietrich");
-      /*
+        System.out.println("Hello from Dietrich");
+
         Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/hello")
+                .get("https://playground.learnqa.ru/api/get_text")
                 .andReturn();
         response.prettyPrint();
-*/
+
     }
 }
